97379617cebf0a213cf8f88e302984bbccb4e7c2
Espen Nilsen <espen.k.nilsen@gmail.com>
2018-08-01 15:03:38 +0200

    skrevet om til klasse

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <title>Realtime Client</title>
 
-    <script src="js/autobahn.js" type="text/javascript"></script>
+    <script src="js/autobahn.alt.js" type="text/javascript"></script>
     
 </head>
 
@@ -16,127 +16,129 @@
 <script>
 
 
-var wamp = {
+var wamp = (function (autobhanInstance) {
 
-    wsuri : 'ws://127.0.0.1:8080',
+    return {
 
-    ws_not_supported : false,
-    ab_sess : null,
+        wsuri : 'ws://127.0.0.1:8080',
 
-    timers : {'ab_reconnect' : null},
+        ws_not_supported : false,
+        ab_sess : null,
 
-    last_topic : null,
-    last_data : null,
-    last_topic_time : null,
-    link : 'down',
-    last_data : null,
-    last_error : null,
-    last_rpc_error : null,
+        timers : {'ab_reconnect' : null},
 
-    subscriber_handler : function(topic, data) {
-        // Data vi mottar her forteller vidre hvordan vi skal fortsette presentasjonen
-        console.log('New article published to category "' + topic + '"');
-        
-        this.last_topic  = topic;
-        this.last_data   = data;
-        
-    },
+        last_topic : null,
+        last_data : null,
+        last_topic_time : null,
+        link : 'down',
+        last_data : null,
+        last_error : null,
+        last_rpc_error : null,
 
-    ab_reconnect_timer : function() {
-        if (this.timers.ab_reconnect == null) {        
-            this.timers.ab_reconnect = setInterval(this.ab_reconnect_function, 5000);
-        }
-        
-        return;
-    },
+        subscriber_handler : function(topic, data) {
+            // Data vi mottar her forteller vidre hvordan vi skal fortsette presentasjonen
+            console.log('New article published to category "' + topic + '"');
+            
+            this.last_topic  = topic;
+            this.last_data   = data;
+            
+        },
 
-    stopTimer : function(timername) {
-        if (typeof this.timers.timername == "undefined") {
+        ab_reconnect_timer : function() {
+            if (this.timers.ab_reconnect == null) {        
+                this.timers.ab_reconnect = setInterval(this.ab_reconnect_function, 5000);
+            }
+            
             return;
-        }
-        
-        clearInterval(this.timers.timername);
-        this.timers.timername = null;
-    },
+        },
 
-    ab_reconnect_function : function() {
-        if (this.ws_not_supported == true) {
-            return;
-        }
+        stopTimer : function(timername) {
+            if (typeof this.timers.timername == "undefined") {
+                return;
+            }
             
-        this.ABStart();
-    },
+            clearInterval(this.timers.timername);
+            this.timers.timername = null;
+        },
 
-    ABStart : function(other_wsuri) {
-        
-        if("WebSocket" in window) {
-            ws_not_supported = false;
+        ab_reconnect_function : function() {
+            if (this.ws_not_supported == true) {
+                return;
+            }
+                
+            this.ABStart();
+        },
 
-        } else {
-            // The browser doesn't support WebSocket - Do you need to enable it?
-            console.console.error("WebSocket NOT supported by your Browser!");
-            ws_not_supported = true;
-            return;
+        ABStart : function(other_wsuri) {
+            
+            let connect_wsuri = '';
+            if (typeof other_wsuri != "undefined") {
+                connect_wsuri = other_wsuri;
+
+            } else {
+                connect_wsuri = this.wsuri;
+            }
+
+            
+            if("WebSocket" in window) {
+                this.ws_not_supported = false;
+
+            } else {
+                // The browser doesn't support WebSocket - Do you need to enable it?
+                console.console.error("WebSocket NOT supported by your Browser!");
+                this.ws_not_supported = true;
+                return;
+            }
+
+            
+            if (typeof this.ab_sess == "object" && this.ab_sess != null && typeof this.ab_sess._wsuri != "undefined" ) {
+                this.ab_sess.close();
+            }
+
+                
+            console.log(autobhanInstance);
+
+            autobhanInstance.Session(connect_wsuri, 
+                function (session) { // WAMP session was established
+                
+                    wamp.link = 'up';
+                    
+                    if (wamp.timers.ab_reconnect != null) {
+                        clearInterval(wamp.timers.ab_reconnect);
+                        wamp.timers.ab_reconnect = null;
+                    }
+                    
+                
+                    wamp.ab_sess = session;
+
+                    // Register all topics to subscribe to
+                    wamp.ab_sess.subscribe('GPS', wamp.subscriber_handler);
+                
+                }, function (code, reason) { // WAMP session is gone
+                
+                    // Unreg session
+                    wamp.ab_sess = null;
+                    
+                    // Log reason
+                    console.log(reason);
+                    
+                    // Notify websock closed
+                    console.warn('WebSocket connection closed');
+                    
+                    
+                    wamp.link = 'down';
+                
+                    // Start reconnect timer
+                    wamp.ab_reconnect_timer();
+                    
+                }, { // The session options
+                'maxRetries': 0,
+                'retryDelay': 2000
+            });
+            
         }
-
-        
-        if (typeof ab_sess == "object" && ab_sess != null && typeof ab_sess._wsuri != "undefined" ) {
-            ab_sess.close();
-        }
-
-        var connect_wsuri = '';
-        if (typeof other_wsuri != "undefined") {
-            connect_wsuri = other_wsuri;
-
-        } else {
-            connect_wsuri = wsuri;
-        }	
-
-        if (typeof ab_sess == "object" && ab_sess != null && typeof ab_sess._wsuri != "undefined" ) {
-            ab_sess.close();
-        }
-
-        ab.connect(connect_wsuri, 
-            function (session) { // WAMP session was established
-            
-                link = 'up';
-                
-                if (timers.ab_reconnect != null) {
-                    clearInterval(timers.ab_reconnect);
-                    timers.ab_reconnect = null;
-                }
-                
-                //Save session to global var
-                ab_sess = session;
-
-                // Register all topics to subscribe to
-                ab_sess.subscribe('GPS', subscriber_handler);
-            
-            }, function (code, reason) { // WAMP session is gone
-            
-                // Unreg session
-                ab_sess = null;
-                
-                // Log reason
-                console.log(reason);
-                
-                // Notify websock closed
-                console.warn('WebSocket connection closed');
-                
-                
-                link = 'down';
-            
-                // Start reconnect timer
-                ab_reconnect_timer();
-                
-            }, { // The session options
-            'maxRetries': 0,
-            'retryDelay': 2000
-        });
-        
     }
-
-};
+})(window.ab);
 
 
 
